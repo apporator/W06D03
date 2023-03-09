b8040e2611214678132d429075edd8aa35756cb1
bird <mbird3@gmail.com>
2023-03-09 09:44:10 -0800

    complete the routing project

--- routes_practice/app/controllers/users_controller.rb
+++ routes_practice/app/controllers/users_controller.rb
@@ -13,18 +13,27 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find(params[:id])
+        user = User.find_by(id: params[:id])
         puts user
-        debugger
+        # debugger
         if user
             render json: user
         else
-            render json: user.errors.full_messages, status: :unprocessable_entity
+            render plain: "404 Not Found", status: :not_found
         end
     end
 
     def update
-        # user = User.find(params[:id])
+        user = User.find_by(id: params[:id])
+
+        if user
+            user.name = user_params[:name] || user.name
+            user.email = user_params[:email] || user.name
+            user.save
+            render json: user
+        else
+            render plain: "404 Not Found", status: :not_found
+        end
     end
 
     def user_params
