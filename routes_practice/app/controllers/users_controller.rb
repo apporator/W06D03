1733035dfb47588822e49ebbe62be764b6ae292d
Bilal <bilalachoudhry@gmail.com>
2023-03-08 17:32:21 -0800

    changes

--- routes_practice/app/controllers/users_controller.rb
+++ routes_practice/app/controllers/users_controller.rb
@@ -4,13 +4,31 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(params.require(:user).permit(:email, :name))
-        user.save!
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user 
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end        
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        puts user
+        debugger
+        if user
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        # user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :name, :id)
     end
 
 end
